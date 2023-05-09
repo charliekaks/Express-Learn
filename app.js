cb734d839c39601194fbf602b07d26cd68b3d1fe
charles Kakai <kaksunchained@gmail.com>
2023-05-09 14:55:49 +0300

    Setting up the server and the controllers and routes for the tasks in the app.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const taskRoutes = require('./routes/task');
 
 
 app.get('/hello',(req, res)=>{
     res.send("Hello World")
 })
+
+
+//Middleware to get json data
+app.use(express.json());
+
+//routes
+app.use('/api/v1/tasks',taskRoutes);
+
+
 
 const port = 3000;
 
---
+++ controller/tasks.js
@@ -0,0 +1,8 @@
+const getAllTasks = (req, res)=>{
+res.send("All Items")
+}
+
+
+module.exports ={
+    getAllTasks,
+}
